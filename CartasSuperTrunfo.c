44f41e239e8256a98d074e33432b04980411a623
julianaaragaoc <julianaaragaoc@gmail.com>
2025-11-12 01:46:03 +0000

    v1 super trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,7 +7,7 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
     char estado1, estado2;  
-    char codigo1[4], codigo2[];
+    char codigo1[4], codigo2[4];
     char nomeCidade1[50], nomeCidade2[50];
     int populacao1, populacao2;
     float area1, area2, pib1, pib2;
